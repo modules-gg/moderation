8407ea6aa06f15f915b9642b91387849595d999e
private09773 <tempt0576@gmail.com>
2026-07-22 11:04:03 +0800

    Create docs/init.py

---
+++ docs/init.py
@@ -0,0 +1,6 @@
+"""Documentation package for the moderation module.
+
+This package contains documentation files, guides, and references
+for the moderation module. It is not imported at runtime.
+
+"""
